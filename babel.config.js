3e5850279e87e44a2606d02e4e6b70719ddb6d9b
Heloise Katharine <44498658+HeloiseKatharine@users.noreply.github.com>
2022-07-30 03:31:26 +0000

    Initial commit

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ["next/babel"],
+  plugins: ["styled-components"]
+}
+
+// plugins: [["styled-components"], { "ssr": true}]
+
+  /*presets: [['next/babel', { 'preset-react': { runtime: 'automatic' } }]],
+  plugins: [
+    'babel-plugin-macros',
+    ['babel-plugin-styled-components', { ssr: true }],
+  ],*/
---
+++ scr/pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import React from 'react'
+
+// const Home: NextPage = () => {
+function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main >
+        <h1 >
+          Hello world!
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
